d0f2ed59ee9b099dfab4ca388edd0ffb55b3dafd
mutugicollins54 <mutugicollins54@gmail.com>
2024-08-04 20:19:44 +0000

    fix: removes riverpod, adds flutter_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod/riverpod.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'widgets/notebookpainter.dart';
 
 void main() {
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
